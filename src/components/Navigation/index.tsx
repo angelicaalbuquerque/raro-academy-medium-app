6e58c116b04d6480896368e715a563a5ed70d63a
angelicaalbuquerque <angelica.o.albuquerque@gmail.com>
2022-04-02 16:19:45 -0300

    Criação botão logout

--- src/components/Navigation/index.tsx
+++ src/components/Navigation/index.tsx
@@ -7,6 +7,7 @@ export const Navigation = () => {
       <Link to="/artigos">Meus Artigos</Link>
       <Link to="/artigos/novo">Novo Artigo</Link>
       <Link to="/login">Login</Link>
+      <Link to="/">Logout</Link>
     </>
   );
 };

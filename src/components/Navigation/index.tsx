05564ed48d403e80256d90690b8bd0f12be4befc
angelicaalbuquerque <angelica.o.albuquerque@gmail.com>
2022-04-03 18:15:06 -0300

    Criação de navegações customizadas

--- src/components/Navigation/index.tsx
+++ src/components/Navigation/index.tsx
@@ -1,13 +1,28 @@
+import { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 
 export const Navigation = () => {
+  const [isAuthenticated, setAuthenticated] = useState(false);
+  useEffect(() => {
+    setAuthenticated(localStorage.getItem("access_token") !== null);
+  }, []);
+
+  if (!isAuthenticated) {
+    return (
+      <>
+        <Link to="/">Home</Link>
+        <Link to="/login">Login</Link>
+      </>
+    );
+  }
+
   return (
     <>
       <Link to="/">Home</Link>
       <Link to="/artigos">Meus Artigos</Link>
       <Link to="/artigos/novo">Novo Artigo</Link>
       <Link to="/login">Login</Link>
-      <Link to="/">Logout</Link>
+      <Link to="/">Sair</Link>
     </>
   );
 };
